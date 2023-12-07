2f10570a38e205104b307ac5dd37072b37030fd5
vlad Amirov <vladamirov@list.ru>
2023-12-07 18:10:10 +0300

    доработка функционала класса hh.ru

--- src/engine.py
+++ src/engine.py
@@ -1,3 +1,4 @@
+import json
 from abc import ABC, abstractmethod
 
 import requests
@@ -33,6 +34,38 @@ class HH(ApiService):
             params = {"text": {self.vacancies}, "area": 113, "per_page": 20, "page": num}
             response = requests.get(url, params=params)
             info = response.json()
+            if info is None:
+                return "Данны не найдены"
+            elif "errors" in info:
+                return info["errors"][0]["value"]
+            elif "items" not in info:
+                return "Нет вакансий"
+            else:
+                for vacancy in range(20):
+                    self.vacancies_all.append(vacancy)
+                    if info["items"][vacancy] is not None \
+                            and info["items"][vacancy]["salary"]["currency"] == "RUR":
+                        self.vacancies.append([info["items"][vacancy]["employer"]["name"],
+                                              info["items"][vacancy]["name"],
+                                              info["items"][vacancy]["apply_alternate_url"],
+                                              info["items"][vacancy]["snippet"]["requirement"],
+                                              info["items"][vacancy]["salary"]["from"],
+                                              info["items"][vacancy]["salary"]["to"]])
+        for vacancy in self.vacancies:
+            vacancies_dict = {"employer": vacancy[0], "name": vacancy[1], "url": vacancy[2], "requirement": vacancy[3],
+                              "salary_from": vacancy[4], "salary_to": vacancy[5]}
+            if vacancies_dict["salary_from"] is None:
+                vacancies_dict["salary_from"] = 0
+            elif vacancies_dict["salary_to"] is None:
+                vacancies_dict["salary_to"] = vacancies_dict["salary_from"]
+            self.vacancies_dict.append(vacancies_dict)
+
+        with open(f"{self.vacancies}_hh_ru.json" "w", encoding="UTF-8") as file:
+            json.dump(self.vacancies_dict, file, indent=4, ensure_ascii=False)
+        print(f"Отбор осуществлялся из {len(self.vacancies_all)} вакансий(проверка обращения к сервису)")
+        return self.vacancies_dict
+
+                    
 
     def get_connector(self):
         pass
